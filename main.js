52c5e5c56c8831fe0132f0bafa68206b9d0143af
Shariful4447 <shariful.islam4447@gmail.com>
2023-03-02 06:26:13 +0600

    improve function done for increment

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ document.getElementById("case-increase").addEventListener("click",function(){
     var caseNewCount = caseCount+1;
     caseInput.value = caseNewCount;
 
-    const price = document.getElementById("price").innerText;
-    const mobilePrice = parseInt(price);
-    const totalPrice = caseNewCount * mobilePrice;
-    document.getElementById("price").innerText=totalPrice;
+    // const price = document.getElementById("price").innerText;
+    // const mobilePrice = parseInt(price);
+    const totalPrice = caseNewCount * 1219;
+    document.getElementById("mobile-price").innerText=totalPrice;
+
     
 
 
@@ -20,4 +21,69 @@ document.getElementById("case-increase").addEventListener("click",function(){
     
 
 })
+
+document.getElementById("case-decrease").addEventListener("click",function(){
+    
+    
+   
+    var caseInput = document.getElementById("case-count");
+    var caseCount = parseInt(caseInput.value);
+    var caseNewCount = caseCount-1;
+    caseInput.value = caseNewCount;
+
+    // var price = document.getElementById("price").innerText;
+    // var totalPriceN =  parseInt(price);
+    const newTotalPrice= caseNewCount * 1219;
+    document.getElementById("mobile-price").innerText=newTotalPrice;
+    
+
+
+
+   
+    
+    
+
+})
+
+
+
+
+// document.getElementById("case-increase-case").addEventListener("click",function(){
+    
+//    handleChange(true); 
+   
+// })
+
+// document.getElementById("case-decrease-case").addEventListener("click",function(){
+    
+    
+//    handleChange(false);
+    
+// })
+function handleChange(isIncrease){
+    var caseInput = document.getElementById("case-count-case");
+    var caseCount = parseInt(caseInput.value);
+    let caseNewCount=caseCount;
+    if(isIncrease == true){
+        caseNewCount = caseCount + 1;
+    }
+    if(isIncrease == false && caseCount>0){
+        caseNewCount = caseCount - 1;
+    }
+    caseInput.value = caseNewCount;
+
+    // var price = document.getElementById("price").innerText;
+    // var totalPriceN =  parseInt(price);
+    const newTotalPrice= caseNewCount * 59;
+    document.getElementById("case-price").innerText=newTotalPrice;
+
+}
+
+
+
+
+
+
+
+
 
